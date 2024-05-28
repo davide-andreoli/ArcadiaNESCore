bc420cf827293a7f1dc4875b73bbe716f5cb1367
Davide Andreoli <davide.andreoli@icloud.com>
2024-05-28 12:31:20 +0200

    Initial Commit

---
+++ ArcadiaNESCore/ArcadiaNESCore.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaNESCore.h
+//  ArcadiaNESCore
+//
+//  Created by Davide Andreoli on 28/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaNESCore.
+FOUNDATION_EXPORT double ArcadiaNESCoreVersionNumber;
+
+//! Project version string for ArcadiaNESCore.
+FOUNDATION_EXPORT const unsigned char ArcadiaNESCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaNESCore/PublicHeader.h>
+
+
