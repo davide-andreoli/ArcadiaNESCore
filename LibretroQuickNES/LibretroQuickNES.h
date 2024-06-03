02a889548209e3108ed319d18902627b2072d816
Davide Andreoli <davide.andreoli@icloud.com>
2024-06-03 08:06:47 +0200

    removed static

--- LibretroQuickNES/LibretroQuickNES.h
+++
@@ -1,9 +0,0 @@
-//
-//  LibretroQuickNES.h
-//  LibretroQuickNES
-//
-//  Created by Davide Andreoli on 29/05/24.
-//
-
-#import <Foundation/Foundation.h>
-#include "libretro.h"
--- LibretroQuickNES/LibretroQuickNES.m
+++
@@ -1,8 +0,0 @@
-//
-//  LibretroQuickNES.m
-//  LibretroQuickNES
-//
-//  Created by Davide Andreoli on 29/05/24.
-//
-
-#import "LibretroQuickNES.h"
